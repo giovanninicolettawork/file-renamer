f3f03bb312c3d04488a1e604f3cfd273d54fddb1
giovanninicolettawork <giovanni.nicoletta.work@gmail.com>
2023-01-28 18:08:34 +0100

    first commit

---
+++ RenameFileInFolder/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace RenameFileInFolder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.stringBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // folderBox
+            // 
+            this.folderBox.Location = new System.Drawing.Point(12, 66);
+            this.folderBox.Name = "folderBox";
+            this.folderBox.Size = new System.Drawing.Size(756, 20);
+            this.folderBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cartella in cui si trovano i file da rinominare";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(251, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Rimuovi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Remove);
+            // 
+            // stringBox
+            // 
+            this.stringBox.Location = new System.Drawing.Point(13, 143);
+            this.stringBox.Name = "stringBox";
+            this.stringBox.Size = new System.Drawing.Size(755, 20);
+            this.stringBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(213, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Testo da aggiungere/eliminare dal nome file";
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(16, 237);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultBox.Size = new System.Drawing.Size(760, 175);
+            this.resultBox.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Aggiungi (prima)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.AddAtInit);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(135, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Aggiungi (dopo)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.AddAtEnd);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.stringBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.folderBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FileRenamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox folderBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox stringBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ RenameFileInFolder/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RenameFileInFolder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Remove(object sender, EventArgs e)
+        {
+            FileInfo[] files = GetFilesInFolder(out string replaceString);
+
+            foreach (FileInfo f in files)
+            {
+                if (f.Name.Contains(replaceString))
+                {
+                    File.Move(f.FullName, f.FullName.Replace(replaceString, ""));
+                    resultBox.Text += "File modificato: " + f.Name + Environment.NewLine;
+                }
+
+            }
+
+        }
+
+
+
+        private void AddAtEnd(object sender, EventArgs e)
+        {
+            FileInfo[] files = GetFilesInFolder(out string replaceString);
+
+            foreach (FileInfo f in files)
+            {
+                {
+                    string extension = Path.GetExtension(f.Name);
+                    string newName = f.Name.Replace(extension, "") + replaceString + extension;
+                    File.Move(f.FullName, Path.Combine(f.DirectoryName, newName));
+                    resultBox.Text += "File modificato: " + f.Name + Environment.NewLine;
+                }
+            }
+        }
+
+        private void AddAtInit(object sender, EventArgs e)
+        {
+            FileInfo[] files = GetFilesInFolder(out string replaceString);
+
+            foreach (FileInfo f in files)
+            {
+                File.Move(f.FullName, Path.Combine(f.DirectoryName, replaceString + f.Name));
+                resultBox.Text += "File modificato: " + f.Name + Environment.NewLine;
+            }
+        }
+
+        private FileInfo[] GetFilesInFolder(out string replaceString)
+        {
+            resultBox.Text = "";
+            string folderPath = folderBox.Text;
+            replaceString = stringBox.Text;
+
+            if (String.IsNullOrEmpty(folderPath) || String.IsNullOrEmpty(replaceString))
+            {
+                resultBox.Text = "Compilare i campi!";
+                return new FileInfo[0];
+            }
+            DirectoryInfo d = new DirectoryInfo(folderPath);
+            return d.GetFiles("*", SearchOption.TopDirectoryOnly);
+
+        }
+    }
+}
